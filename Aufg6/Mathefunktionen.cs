9a2e5585542a3f5d4d94ae903709ac85d4093aaf
Justin <justin@kauz.ch>
2022-05-12 13:54:04 +0200

    Aufg6

---
+++ Aufg6/Mathefunktionen.cs
@@ -0,0 +1,41 @@
+﻿namespace Aufg6
+{
+    public class Mathefunktionen
+    {
+        public int getGgT(int x, int y)
+        {
+            int rest = 0;
+            while(true) {
+                rest = x % y;
+                if (rest == 0) {
+                    break;
+                }
+                x = y;
+                y = rest;
+            }
+            return y;
+        }
+        /// <summary>
+        /// Berechnet ggT in While Schleife
+        /// nutzt ggT, um KgV zu berechnen
+        /// (z1 * z2) / ggT = KgV
+        /// </summary>
+        /// <param name="x">erste Zahl</param>
+        /// <param name="y">zweite Zahl / ggT</param>
+        /// <returns></returns>
+        public int getKgV(int x, int y)
+        {
+            int rest = 0, z1 = x, z2 = y; // originalwerte speichern
+            while (true) {
+                rest = x % y;
+                if (rest == 0) {
+                    break;
+                }
+                x = y;
+                y = rest;
+            }
+            int ggT = y;    // ggT weitergeben
+            return (z1 * z2) / ggT; // originalwerte zur berechnung benutzen
+        }
+    }
+}
---
+++ Aufg6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Aufg6
+{
+    class Program
+    {
+        /*
+         * Aufg6
+         * Zeit: 30'
+         * 
+         * Baue ihn so um, dass ggT und kgV unabhängig voneinander auf Methoden (Funktionen) in einer anderen Datei (Class) zugreifen.
+         * Gehe dazu auf dein Projekt und erstelle eine neue Class und importiere sie:
+         * Mathefunktionen mathe = new Mathefunktionen();
+         * Damit du in deinem Code auf die Methoden (Funktionen) zugreifen kannst, musst du sie so: mathe.getGgT(); aufrufen.
+         * Damit die Methoden (Funktionen) und die Class aufgerufen werden können, muss ein public vor ihnen stehen.
+         */
+        static void Main(string[] args)
+        {
+            // eingene Class importieren
+            Mathefunktionen mathe = new Mathefunktionen();
+            Console.WriteLine("Aufgabe 06: Mathefunktionen");
+            Console.WriteLine("*************************************");
+            int zahl1 = 0, zahl2 = 0, ggT = 0, kgV = 0;
+            do {
+                Console.Write("Zahl 1 eingeben: ");
+            } while (int.TryParse(Console.ReadLine(), out zahl1) == false);
+            zahl1 = Math.Abs(zahl1); //Vorzeichen weglassen (Absolutwert)
+            do {
+                Console.Write("Zahl 2 eingeben: ");
+            } while (int.TryParse(Console.ReadLine(), out zahl2) == false);
+            zahl2 = Math.Abs(zahl2); //Vorzeichen weglassen (Absolutwert)
+            string ausgabeText = "Der {0} der beiden Zahlen " + zahl1 + " und " + zahl2 + " ist: {1}";
+            ggT = mathe.getGgT(zahl1, zahl2);
+            kgV = mathe.getKgV(zahl1, zahl2);
+            Console.WriteLine(ausgabeText, "ggT", ggT);
+            Console.WriteLine(ausgabeText, "kgV", kgV);
+            Console.ReadKey();
+        }
+    }
+}
