117860a61f39217824cb7a6951359193b5c07761
Justin <justin@kauz.ch>
2022-05-12 13:56:24 +0200

    Aufg16

---
+++ Aufg16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufg16
+{
+    class Program
+    {
+        /** Aufg16
+         * Zeit: 15 min
+         * 
+         * Schreibe ein Programm, das normalen Zahlen in eine binäre Zahl umwandeln.
+         * Die binäre Zahl soll immer mit mind. 8 Stellen dargestellt werden. Z.B. 50 = 00110010
+         * 
+         */
+        static void Main(string[] args)
+        {
+            int number = 0;
+            do {
+                Console.Write("Geben Sie eine ganze Zahl ein: ");
+            } while (int.TryParse(Console.ReadLine(), out number) == false);    
+
+            Console.WriteLine("Die ganze Zahl als Binärzahl: {0}", Convert.ToString(number, 2).PadLeft(8, '0')); // methode 1
+
+            Console.ReadLine();  // endl
+        }
+    }
+}
